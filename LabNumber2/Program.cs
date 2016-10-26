bbba0617a356773bb704f889643dae07d8ca493c
thumbaleena <thumbaleena@outlook.com>
2016-10-26 11:44:54 -0400

    removed extra lines from validation

--- LabNumber2/Program.cs
+++ LabNumber2/Program.cs
@@ -10,7 +10,7 @@ namespace LabNumber2
     {
         static void Main(string[] args)
         {
-        string playerName, choice1, choice2, dragonHeads, weapon, dragonEyes, dragonName;
+        string playerName, choice1, choice2, dragonHeads, weapon, dragonEyes, dragonName; //declare variables
 
         Console.Write("Welcome! What is your name? (enter your name): ");
         playerName = Console.ReadLine();
@@ -20,36 +20,36 @@ namespace LabNumber2
             choice1 = Console.ReadLine();
             Console.WriteLine();
 
-            switch (choice1)
+            switch (choice1)  //logic based on answer
             {
                 case "yes":
-                    Console.WriteLine("Excellent!");
+                    Console.WriteLine("Excellent!"); //keep going
                     Console.WriteLine();
                     break;
                 case "no":
-                    Console.WriteLine("Well, all right then. Good-bye.");
+                    Console.WriteLine("Well, all right then. Good-bye."); //terminate the program
                     Console.WriteLine("Press any key to exit.");
                     Console.ReadKey();
                     Environment.Exit(0);
                     break;
                 default:
-                    Console.Write("Invalid entry. Try again! ");
+                    Console.Write("Invalid entry. Try again! ");  //error message
                     choice1 = Console.ReadLine();
                     break;
             }
-            while (choice1 != "yes" && choice1 != "no")
+            while (choice1.ToLower() != "yes" && choice1.ToLower() != "no") //if they don't enter yes or now, write error message until they do
             {
                 Console.Write("Invalid entry. Try again! ");
                 choice1 = Console.ReadLine();
             }
-            if (choice1 == "no")
+            if (choice1 == "no")  //if they choose no after running into the error message, terminate program
             {
                 Console.WriteLine("Well, all right then. Good-bye.");
                 Console.WriteLine("Press any key to exit.");
                 Console.ReadKey();
                 Environment.Exit(0);
             }
-            Console.WriteLine("You are walking across a field and you encounter a fire-breathing dragon!  You can: (choose number) ");
+            Console.WriteLine("You are walking across a field and you encounter a fire-breathing dragon!  You can: (choose number) "); //continue the story
             Console.WriteLine();
             Console.WriteLine(" 1. Face the beast!");
             Console.WriteLine(" 2. Run Away!");
@@ -82,7 +82,7 @@ namespace LabNumber2
             Console.WriteLine("Staff");
             Console.WriteLine();
             weapon = Console.ReadLine();
-            while (weapon != "bow" && weapon != "sword" && weapon != "staff" && weapon != "Sword" && weapon != "Staff" && weapon != "Bow")
+            while (weapon.ToLower() != "bow" && weapon.ToLower() != "sword" && weapon.ToLower() != "staff")
             {
                 Console.Write("Invalid entry. Try again! ");
                 weapon = Console.ReadLine();
@@ -96,7 +96,7 @@ namespace LabNumber2
             Console.WriteLine("Blue");
             Console.WriteLine();
             dragonEyes = Console.ReadLine();
-            while (dragonEyes != "red" && dragonEyes != "blue" && dragonEyes != "Red" && dragonEyes != "Blue")
+            while (dragonEyes.ToLower() != "red" && dragonEyes.ToLower() != "blue")
             {
                 Console.Write("Invalid entry. Try again! ");
                 dragonEyes = Console.ReadLine();
